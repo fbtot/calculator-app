7fd705a3ff70e0756ad2d59b00e38d35509f2345
Filippo Bristot <filippo.bristot@hotmail.it>
2022-02-15 12:18:30 +0100

    build(webpack): add html plugin and dev script

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
 
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  mode,
   entry: './src/js/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
