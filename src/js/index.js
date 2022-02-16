c51db582873bdfb5b83887c1536f88389bae43ae
Filippo Bristot <filippo.bristot@hotmail.it>
2022-02-16 08:38:14 +0100

    feat(js): add temporary layout switch function

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 import '../scss/main.scss';
+
+const scientificModeSwitch = document.getElementById('scientificModeSwitch');
+const calculator = document.getElementById('calculator');
+
+scientificModeSwitch.addEventListener('click', () => {
+  const calculatorClassList = calculator.classList;
+
+  if (calculatorClassList.contains('normal-layout')) {
+    calculatorClassList.remove('normal-layout');
+    calculatorClassList.add('scientific-layout');
+  } else if (calculatorClassList.contains('scientific-layout')) {
+    calculatorClassList.remove('scientific-layout');
+    calculatorClassList.add('normal-layout');
+  }
+});
