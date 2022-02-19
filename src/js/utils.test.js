c0615854c0b5fa513636a936dcd9c6d289524d2f
Filippo Bristot <filippo.bristot@hotmail.it>
2022-02-19 14:14:11 +0100

    test: delete tests

--- src/js/utils.test.js
+++
@@ -1,9 +0,0 @@
-import { nextElement } from './utils';
-
-test('should retrieve the element next to the one provided', () => {
-  const arr = ['moose', 'rooster', 'bear'];
-
-  expect(nextElement('moose', arr)).toBe('rooster');
-  expect(nextElement('bear', arr)).toBe('moose');
-  expect(nextElement('eagle', arr)).toBe('');
-});
