0dace87ff0f1b7ae588466060f3f6095273311b9
Filippo Bristot <filippo.bristot@hotmail.it>
2022-02-19 19:20:32 +0100

    feat(js): add buttonsObj

---
+++ src/js/buttonsObj.js
@@ -0,0 +1,253 @@
+// TODO: le funzionalità dei tasti per quanto riguarda MathJS è meglio farle con dei metidi. Forse ancora meglio usare una classe
+buttonObj = {
+  openParenthesis: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  closeParenthesis: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  cancelMemory: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  addToMemory: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  subtractFromMemory: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  recallMemory: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  seven: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  eight: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  nine: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  delete: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  secondFunction: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  powerOfTwo: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  powerOfThree: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  powerOfY: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  eToX: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  tenX: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  four: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  five: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  six: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  plus: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  inverse: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  squareRoot: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  cubicRoot: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  yRoot: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  in: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  logTen: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  one: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  two: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  three: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  minus: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  factorial: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  sin: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  cos: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  tan: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  eulersNumber: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  exponentialNotation: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  comma: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  zero: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  divide: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  multiply: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  radial: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  sinh: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  cosh: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  tanh: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  pi: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  random: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  percentage: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  invertNumber: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  reset: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+  equal: {
+    mathjs: '',
+    scientific: true,
+    display: '',
+  },
+};
