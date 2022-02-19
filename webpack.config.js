c618e2aee2f531e5478587040ccfb206a057473c
Filippo Bristot <filippo.bristot@hotmail.it>
2022-02-19 16:23:24 +0100

    build(webpack): add mp3 support

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.mp3$/,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
