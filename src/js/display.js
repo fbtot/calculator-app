cc56fdb7a0ea2cdab3bfcaa65c33b8f40383dfae
Filippo Bristot <filippo.bristot@hotmail.it>
2022-02-20 08:27:56 +0100

    refactor(js): remove console.log

--- src/js/display.js
+++ src/js/display.js
@@ -3,7 +3,6 @@ import { buttonObj } from './buttonsObj';
 const displayContentEl = document.getElementById('displayContent');
 
 function displayContent(id) {
-  console.log(buttonObj[id].mathjs);
   if (displayContentEl.innerText === '0') displayContentEl.innerText = buttonObj[id].mathjs;
   else displayContentEl.innerText += buttonObj[id].mathjs;
 }
