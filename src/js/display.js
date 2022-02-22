843596703d9bf853000c49c7bf8a821a687eb4e8
Filippo Bristot <filippo.bristot@hotmail.it>
2022-02-22 10:00:02 +0100

    feat(js): add highlight display functionality

--- src/js/display.js
+++ src/js/display.js
@@ -4,22 +4,25 @@ import { calculatorObj } from './doTheMath';
 const displayContentEl = document.getElementById('displayContent');
 
 function addDisplayContent(id) {
-  displayContentEl.innerText += buttonObj[id].mathjs;
+  displayContentEl.innerHTML += buttonObj[id].mathjs;
 }
 
 function replaceDisplayContent(id) {
-  displayContentEl.innerText += buttonObj[id].mathjs;
+  displayContentEl.innerHTML += buttonObj[id].mathjs;
 }
 
 function updateDisplay(string) {
-  if (string) displayContentEl.innerText = formatString(string);
-  else if (calculatorObj.operationArr.length === 0) displayContentEl.innerText = 0;
-  else displayContentEl.innerText = formatString(calculatorObj.operationArr.join(''));
+  if (string) displayContentEl.innerHTML = formatString(string);
+  else if (calculatorObj.operationArr.length === 0) displayContentEl.innerHTML = 0;
+  else displayContentEl.innerHTML = formatString(calculatorObj.operationArr.join(''));
 }
 
 // TODO: la regex non va bene, mostra la dvisione tra le migliaia anche dopo la virgola.
 function formatString(string) {
-  return String(string).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return String(string)
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    .replaceAll('(', '<span class="highlight--bg">(')
+    .replaceAll(')', ')</span>');
 }
 
 export { addDisplayContent, replaceDisplayContent, updateDisplay };
